8eb1a889ea7ad46083a6e7f793b6d6b9fdcbcf31
unknown <Minh Tuyen>
2022-03-16 21:09:36 +0700

    upload du lieu

--- formnhap.php
+++ formnhap.php
@@ -6,7 +6,6 @@
 Name: <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 Birthday: <input type="date" name="birth"><br>
-Password: <input type="password" name="pass"><br>
 
 <input type="submit">
 </form>
